f02f6d3be3df1b59fad70d226778b1ae91baf090
Nima Nowroozi <nowrouzi.nima@gmail.com>
2016-03-06 21:16:24 +0330

    using Standard Scaler to normalizing unit

---
+++ two_nearest_neighbors.py
@@ -0,0 +1,24 @@
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+import numpy as np
+import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.cross_validation import KFold
+
+
+df = pd.read_pickle('processed_data/dataframe.pd')
+labels = df["Status"].values
+del df['Status']
+features = df[list(df.columns)].values
+kf = KFold(len(features), n_folds=5, shuffle=True)
+classifier = KNeighborsClassifier(n_neighbors=1)
+classifier = Pipeline([('norm', StandardScaler()), ('knn', classifier)])
+
+means = []
+for training,testing in kf:
+    classifier.fit(features[training], labels[training])
+    prediction = classifier.predict(features[testing])
+    mean = np.mean(prediction == labels[testing])
+    print 'Fold predicting accuracy mean is: {:.1%}'.format(mean)
+    means.append(mean)
+print('Total Mean accuracy is: {:.1%}'.format(np.mean(means)))
