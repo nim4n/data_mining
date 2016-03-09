8665118a9c699a94c8fb12828dd45ce50b6ac9c1
Nima Nowroozi <nowrouzi.nima@gmail.com>
2016-03-09 20:35:46 +0330

    check new feature with nearest neighbors

--- variance_threshold.py
+++ variance_threshold.py
@@ -1,4 +1,9 @@
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+import numpy as np
 import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.cross_validation import KFold
 from sklearn.feature_selection import VarianceThreshold
 
 df = pd.read_pickle('processed_data/dataframe.pd')
@@ -9,4 +14,17 @@ features = df[list(df.columns)].values
 sel = VarianceThreshold(threshold=(.8 * (1 - .8)))
 new_features = sel.fit_transform(features)
 print len(features[0]), len(new_features[0])
+
+kf = KFold(len(new_features), n_folds=5, shuffle=True)
+classifier = KNeighborsClassifier(n_neighbors=4)
+classifier = Pipeline([('norm', StandardScaler()), ('knn', classifier)])
+
+means = []
+for training, testing in kf:
+    classifier.fit(new_features[training], labels[training])
+    prediction = classifier.predict(new_features[testing])
+    mean = np.mean(prediction == labels[testing])
+    print 'Fold predicting accuracy mean is: {:.1%}'.format(mean)
+    means.append(mean)
+print('Total Mean accuracy is: {:.1%}'.format(np.mean(means)))
 
