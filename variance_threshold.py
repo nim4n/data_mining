23fb34d76a4ad2ed0ae55d4b865b6f57c9dfef4b
Nima Nowroozi <nowrouzi.nima@gmail.com>
2016-03-09 20:04:01 +0330

    removing noise feature

--- variance_threshold.py
+++ variance_threshold.py
@@ -5,3 +5,8 @@ df = pd.read_pickle('processed_data/dataframe.pd')
 labels = df["Status"].values
 del df['Status']
 features = df[list(df.columns)].values
+#define threshold
+sel = VarianceThreshold(threshold=(.8 * (1 - .8)))
+new_features = sel.fit_transform(features)
+print len(features[0]), len(new_features[0])
+
