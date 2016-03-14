c05a190300a51a594e0878b6e4f2a3bfbc334080
Nima <nim4n@yahoo.com>
2016-03-14 18:29:23 +0330

    first try with SVM

--- svm_svc.py
+++ svm_svc.py
@@ -1,12 +1,23 @@
-
-import pandas as pd
 import pickle
+import pandas as pd
+import numpy as np
 from sklearn import svm
+from sklearn.cross_validation import KFold
 
 df = pd.read_pickle('processed_data/correlation_dataframe.pd')
 labels = df["Status"].values
 del df['Status']
 features = df[list(df.columns)].values
-clf = svm.SVC()
-clf.fit(features, labels)
-s = pickle.dumps(clf)
+classifier = svm.SVC()
+classifier.fit(features, labels)
+kf = KFold(len(features), n_folds=5, shuffle=True)
+
+
+means = []
+for training, testing in kf:
+    classifier.fit(features[training], labels[training])
+    prediction = classifier.predict(features[testing])
+    mean = np.mean(prediction == labels[testing])
+    print 'Fold predicting accuracy mean is: {:.1%}'.format(mean)
+    means.append(mean)
+print('Total Mean accuracy is: {:.1%}'.format(np.mean(means)))
