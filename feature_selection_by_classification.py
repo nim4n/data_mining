e655c6eae01c86ac5768457c11efb23baf424f9e
Nima Nowroozi <nowrouzi.nima@gmail.com>
2016-04-01 16:11:07 +0430

    work on classification rank

--- feature_selection_by_classification.py
+++ feature_selection_by_classification.py
@@ -5,7 +5,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.cross_validation import KFold
 from sklearn.feature_selection import VarianceThreshold
-
+'''
 df = pd.read_pickle('processed_data/dataframe_by_std.pd')
 labels = df["Status"].values
 
@@ -13,21 +13,55 @@ kf = KFold(len(labels), n_folds=5, shuffle=True)
 classifier = KNeighborsClassifier(n_neighbors=1)
 status = df['Status']
 del df['Status']
+count = 0
 for column in df.columns:
-    features = df[column].values
-    features = df[list([column])].values
-    #print features
-    means = []
-    for training, testing in kf:
-        classifier.fit(features[training], labels[training])
-        prediction = classifier.predict(features[testing])
-        mean = np.mean(prediction == labels[testing])
-        means.append(mean)
+    try:
+        features = df[column].values
+        features = df[list([column])].values
+        #print features
+        means = []
+        for training, testing in kf:
+            classifier.fit(features[training], labels[training])
+            prediction = classifier.predict(features[testing])
+            mean = np.mean(prediction == labels[testing])
+            means.append(mean)
+        total_mean = np.mean(means)
+        if total_mean > 0.60:
+            count += 1
+            print('Total Mean accuracy is: {:.1%}'.format(total_mean))
+        else:
+            del df[column]
+    except Exception as e:
+        del df[column]
+        print e
 
-    print('Total Mean accuracy is: {:.1%}'.format(np.mean(means)))
+df.to_pickle('processed_data/rank_classification_dataframe.pd')
+print count
 
-
-
-
-
-#df.to_pickle('processed_data/correlation_dataframe.pd')
+'''
+df = pd.read_pickle('processed_data/rank_classification_dataframe.pd')
+print df.shape
+corr_df = df.corr()
+removed_columns = []
+exist_columns = []
+for column in corr_df:
+    try:
+        corr_df.drop(column, inplace=True)
+        max_column_name = corr_df[column].idxmax()
+        if max_column_name in removed_columns:
+            corr_df.drop(max_column_name, inplace=True)
+            print max_column_name
+            max_column_name = corr_df[column].idxmax()
+            print max_column_name
+        max_corr = corr_df[column].max()
+        if max_corr > 0.80 and not column in exist_columns:
+            print max_corr
+            del df[column]
+            removed_columns.append(column)
+            exist_columns.append(max_column_name)
+            print '__________________________________'
+    except Exception as e:
+        print e
+        print corr_df[column]
+df.to_pickle('processed_data/rank_classification_dataframe_remove_relation.pd')
+print df.shape
