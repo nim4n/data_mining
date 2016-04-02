de822ba78582783074b078356944c563ca1c23c0
Nima <nim4n@yahoo.com>
2016-04-02 14:24:46 +0430

    89 - 94 accourate

---
+++ feature_selection_by_random_projection.py
@@ -0,0 +1,33 @@
+import numpy as np
+import pandas as pd
+from sklearn import random_projection
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.cross_validation import KFold
+from sklearn.feature_selection import VarianceThreshold
+
+df = pd.read_pickle('processed_data/rank_classification_dataframe_remove_relation.pd')
+labels = df["Status"].values
+#df = pd.read_pickle('processed_data/rank_classification_dataframe.pd')
+features = df[list(df.columns)].values
+print features.shape
+#transformer = random_projection.GaussianRandomProjection(n_components=82)
+#features = transformer.fit_transform(features)
+print features.shape
+
+
+kf = KFold(len(features), n_folds=7, shuffle=True)
+classifier = KNeighborsClassifier(n_neighbors=1)
+classifier = Pipeline([('norm', StandardScaler()), ('knn', classifier)])
+
+means = []
+for training, testing in kf:
+    classifier.fit(features[training], labels[training])
+    prediction = classifier.predict(features[testing])
+    mean = np.mean(prediction == labels[testing])
+    print 'Fold predicting accuracy mean is: {:.1%}'.format(mean)
+    means.append(mean)
+print('Total Mean accuracy is: {:.1%}'.format(np.mean(means)))
+
+
