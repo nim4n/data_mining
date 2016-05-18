0bf78e62747d33ed8502c50b35e4744b5ca901de
Nima Nowroozi <nowrouzi.nima@gmail.com>
2016-05-18 19:19:41 +0430

    add try except

--- pre_processing.py
+++ pre_processing.py
@@ -5,18 +5,21 @@ import pandas as pd
 df = pd.read_pickle('processed_data/dataframe_lung.pd')
 counter = 0
 for column in df.columns:
-    if column != 'Status':
-        #remove little expression
-        remove_little_expression = df[column] < 20
-        df[column][remove_little_expression] = 20
-        if (df[column].max() - df[column].min()) < 500:
-            del df[column]
-            counter += 1
-        else:
-            print df.shape
-            std = df[column].std()
-            remove_outlier = (df[column] < (2 * std)) & (df[column] > (-2 * std))
-            df[column][~remove_outlier] = 20
+    try:
+        if column != 'Status':
+            #remove little expression
+            remove_little_expression = df[column] < 20
+            df[column][remove_little_expression] = 20
+            if (df[column].max() - df[column].min()) < 500:
+                del df[column]
+                counter += 1
+            else:
+                print df.shape
+                std = df[column].std()
+                remove_outlier = (df[column] < (2 * std)) & (df[column] > (-2 * std))
+                df[column][~remove_outlier] = 20
+    except Exception as e:
+        print e
 
 
 df.to_pickle('processed_data/pre_process_dataframe_lung.pd')
